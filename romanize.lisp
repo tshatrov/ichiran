f5f8b5437dd433c8bd7fedb5b002013c839da01f
Timofei Shatrov <timofei.shatrov@gmail.com>
2014-09-06 10:21:03 +0400

    basic hepburn

--- romanize.lisp
+++ romanize.lisp
@@ -37,22 +37,22 @@
       (cond ((null item)) 
             ((characterp item) (princ item out))
             ((atom item) (princ (r-base method item) out))
-            ((listp item) (princ (r-apply method (car item) (cdr item)) out))))))
+            ((listp item) (princ (r-apply (car item) method (cdr item)) out))))))
   
 (defgeneric r-base (method item)
   (:documentation "Process atomic char class")
   (:method (method item)
     (string-downcase item)))
 
-(defgeneric r-apply (method modifier cc-tree)
+(defgeneric r-apply (modifier method cc-tree)
   (:documentation "Apply modifier to something")
-  (:method (method (modifier (eql :sokuon)) cc-tree)
+  (:method ((modifier (eql :sokuon)) method cc-tree)
     (let ((inner (romanize-core method cc-tree)))
       (if (zerop (length inner)) inner
           (format nil "~a~a" (char inner 0) inner))))
-  (:method (method (modifier (eql :long-vowel)) cc-tree)
+  (:method ((modifier (eql :long-vowel)) method cc-tree)
     (romanize-core method cc-tree))
-  (:method (method (modifier symbol) cc-tree)
+  (:method ((modifier symbol) method cc-tree)
     (format nil "~a~a" (romanize-core method cc-tree) (string-downcase modifier))))
 
 (defgeneric r-simplify (method str)
@@ -68,6 +68,63 @@
 
 (defmethod r-base ((method generic-romanization) item)
   (or (gethash item (kana-table method)) (call-next-method)))
+
+(defmethod r-apply ((modifier symbol) (method generic-romanization) cc-tree)
+  (let ((yoon (gethash modifier (kana-table method))))
+    (if yoon
+        (let ((inner (romanize-core method cc-tree)))
+          (format nil "~a~a" (subseq inner 0 (max 0 (1- (length inner)))) yoon))
+        (call-next-method))))
+
+(hash-from-list *hepburn-kana-table*
+                '(:a "a"      :i "i"      :u "u"      :e "e"      :o "o"
+                  :ka "ka"    :ki "ki"    :ku "ku"    :ke "ke"    :ko "ko"
+                  :sa "sa"    :shi "shi"  :su "su"    :se "se"    :so "so"
+                  :ta "ta"    :chi "chi"  :tsu "tsu"  :te "te"    :to "to"
+                  :na "na"    :ni "ni"    :nu "nu"    :ne "ne"    :no "no"
+                  :ha "ha"    :hi "hi"    :fu "fu"    :he "he"    :ho "ho"
+                  :ma "ma"    :mi "mi"    :mu "mu"    :me "me"    :mo "mo"
+                  :ya "ya"                :yu "yu"                :yo "yo"
+                  :ra "ra"    :ri "ri"    :ru "ru"    :re "re"    :ro "ro"
+                  :wa "wa"    :wi "wi"                :we "we"    :wo "wo"
+                  :n "n'"
+                  :ga "ga"    :gi "gi"    :gu "gu"    :ge "ge"    :go "go"
+                  :za "za"    :ji "ji"    :zu "zu"    :ze "ze"    :zo "zo"
+                  :da "da"    :dji "ji"   :dzu "zu"   :de "de"    :do "do"
+                  :ba "ba"    :bi "bi"    :bu "bu"    :be "be"    :bo "bo"
+                  :pa "pa"    :pi "pi"    :pu "pu"    :pe "pe"    :po "po"
+                  :+a "a"     :+i "i"     :+u "u"     :+e "e"     :+o "o"
+                  :+ya "ya"               :+yu "yu"               :+yo "yo"
+                  ))
+
+(defclass generic-hepburn (generic-romanization)
+  ((kana-table :initform *hepburn-kana-table*)))
+
+(defmethod r-apply ((modifier (eql :+ya)) (method generic-hepburn) cc-tree)
+  (case (car cc-tree)
+    (:shi "sha")
+    (:chi "cha")
+    ((:ji :dji) "ja")
+    (t (call-next-method))))
+
+(defmethod r-apply ((modifier (eql :+yu)) (method generic-hepburn) cc-tree)
+  (case (car cc-tree)
+    (:shi "shu")
+    (:chi "chu")
+    ((:ji :dji) "ju")
+    (t (call-next-method))))
+
+(defmethod r-apply ((modifier (eql :+yo)) (method generic-hepburn) cc-tree)
+  (case (car cc-tree)
+    (:shi "sho")
+    (:chi "cho")
+    ((:ji :dji) "jo")
+    (t (call-next-method))))
+
+(defmethod r-simplify((method generic-hepburn) str)
+  ;; remove apostrophe after n if the next character isn't a vowel
+  (ppcre:regex-replace-all "n'([^aiueoy]|$)" str "n\\1"))
+
 
 (defun romanize-list (cc-list &key (method *default-romanization-method*))
   "Romanize a character class list according to method"
