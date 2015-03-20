3651a854f52f24823d65dc8aed2015d54100bfcd
Timofei Shatrov <timofei.shatrov@gmail.com>
2015-03-20 23:35:07 +0300

    maintenance module

--- ichiran.lisp
+++ ichiran.lisp
@@ -1,6 +1,28 @@
-;;;; ichiran.lisp
+(defpackage :ichiran/maintenance
+  (:nicknames :ichiran/mnt)
+  (:use :cl)
+  (:import-from :ichiran/dict :load-jmdict :load-best-readings
+                :add-errata :recalc-entry-stats)
+  (:import-from :ichiran/kanji :load-kanjidic :load-kanji-stats)
+  (:export
+   #:full-init
+   #:load-jmdict
+   #:load-best-readings
+   #:load-kanjidic
+   #:load-kanji-stats
+   #:add-errata
+   #:recalc-entry-stats
+   ))
 
-(in-package #:ichiran)
+(in-package :ichiran/maintenance)
 
-;;; "ichiran" goes here. Hacks and glory await!
-
+(defun full-init ()
+  (format t "Initializing ichiran/dict...~%")
+  (load-jmdict)
+  (format t "Calculating readings...~%")
+  (load-best-readings)
+  (format t "Initializing ichiran/kanji...~%")
+  (load-kanjidic)
+  (format t "Calculating kanji stats...~%")
+  (load-kanji-stats)
+  )
