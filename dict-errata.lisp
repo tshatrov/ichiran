4ef4bad78697d84a4ff0a511cbf56c43bc2a81e8
Timofei Shatrov <timofei.shatrov@gmail.com>
2015-05-30 20:56:46 +0300

    errata

--- dict-errata.lisp
+++ dict-errata.lisp
@@ -310,6 +310,7 @@
   (set-common 'kana-text 1739410 "わけない" 0)
   (set-common 'kanji-text 1416860 "誰も" 0)
   (set-common 'kana-text 2093030 "そっか" 0)
+  (set-common 'kanji-text 1001840 "お兄ちゃん" 0)
   
   ;; remove sense for なり and make it not root
   (delete-senses 2611370 (constantly t))
