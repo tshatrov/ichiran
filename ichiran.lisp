e35e69ccf6951615e653ec3924c8f54ccffb633c
Timofei Shatrov <timofei.shatrov@gmail.com>
2015-06-17 23:11:33 +0300

    tool to compare queries between databases

--- ichiran.lisp
+++ ichiran.lisp
@@ -1,6 +1,6 @@
 (defpackage :ichiran/maintenance
   (:nicknames :ichiran/mnt)
-  (:use :cl)
+  (:use :cl :postmodern)
   (:import-from :ichiran/dict :load-jmdict :load-best-readings
                 :add-errata :recalc-entry-stats)
   (:import-from :ichiran/kanji :load-kanjidic :load-kanji-stats)
@@ -13,6 +13,7 @@
    #:add-errata
    #:recalc-entry-stats
    #:custom-init
+   #:compare-queries
    ))
 
 (in-package :ichiran/maintenance)
@@ -35,3 +36,14 @@
         (ichiran/dict::*jmdict-data* (or jmdict-data ichiran/dict::*jmdict-data*))
         (ichiran/kanji::*kanjidic-path* (or kanjidic-path ichiran/kanji::*kanjidic-path*)))
     (full-init)))
+
+(defmacro compare-queries (conn1 conn2 &body query)
+  (alexandria:with-gensyms (q1 q2)
+    `(let ((,q1 (with-connection ,conn1 (query ,@query)))
+           (,q2 (with-connection ,conn2 (query ,@query))))
+       (values (set-difference ,q1 ,q2 :test 'equal)
+               (set-difference ,q2 ,q1 :test 'equal)))))
+
+;; example queries
+;; find all [uk] tag changes
+;; (:select 'seq :distinct :from 'sense-prop :where (:and (:= 'tag "misc") (:= 'text "uk")))
