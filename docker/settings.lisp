bc7aa210260b452e4cd07c41a37bde04b880cb0b
Heliozoa <daniel.x.martinez@helsinki.fi>
2023-07-19 17:36:27 +0300

    Allow setting postmodern:*default-use-ssl* with ICHIRAN_SSL

--- docker/settings.lisp
+++ docker/settings.lisp
@@ -1,5 +1,17 @@
 (in-package #:ichiran/conn)
 
+(setf postmodern:*default-use-ssl*
+    (let* ((env-ssl (uiop:getenv "ICHIRAN_SSL")))
+        (if env-ssl
+            (cond
+                ((string= env-ssl "no") :no)
+                ((string= env-ssl "try") :try)
+                ((string= env-ssl "require") :require)
+                ((string= env-ssl "yes") :yes)
+                ((string= env-ssl "full") :full)
+                (t (error (format nil "Invalid environment variable ICHIRAN_SSL=~a. Expected no, try, require, yes or full." env-ssl))))
+            :no)))
+
 (defparameter *connection* '("jmdict0122" "postgres" "password" "pg"))
 
 (defparameter *connections* '((:old "jmdict_old" "postgres" "password" "localhost")
