8abf732c0e60fa05206f0c209ae9e06d208ffb09
Peter T. Brown <peter@flippyhead.com>
2024-11-01 08:04:52 -0700

    don't check postgres

--- docker/ichiran-scripts/entrypoint.sh
+++ docker/ichiran-scripts/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-echo "Checking postgres server status..."
-while : ; do
-    pg_isready -h pg > /dev/null && break;
-    sleep 1;
-done
-
-echo "Postgres is ready, starting main container init."
+echo "Starting main container init."
 init-all;
 
 echo "All set, awaiting commands."
