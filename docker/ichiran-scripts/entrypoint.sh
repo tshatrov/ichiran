2e81a762436b975a9d8899c8863646b03c8ff52e
peterbrown <peter@flippyhead.com>
2024-11-08 15:28:29 +0900

    leave the core alone!

--- docker/ichiran-scripts/entrypoint.sh
+++ docker/ichiran-scripts/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 echo "Starting main container init."
-init-all;
+
+# First run init-sbcl if core doesn't exist
+if [ ! -f /root/ichiran.core ]; then
+    init-sbcl
+fi
+
+# Then start the web server
+init-web
 
 echo "All set, awaiting commands."
 sleep infinity;
