6e245bdd7a3bb185a640e432addb59cbe58f410d
peterbrown <peter@flippyhead.com>
2024-11-09 09:15:10 +0900

    using ros

--- docker/ichiran-scripts/entrypoint.sh
+++ docker/ichiran-scripts/entrypoint.sh
@@ -2,19 +2,8 @@
 
 echo "Starting main container init."
 
-# First run init-sbcl if core doesn't exist
-if [ ! -f /root/ichiran.core ]; then
-    echo "Core file not found, initializing..."
-    init-sbcl
-    if [ ! -f /root/ichiran.core ]; then
-        echo "Failed to create core file!"
-        exit 1
-    fi
-fi
+init-all;
 
-# Then start the web server
-echo "Starting web server with existing core..."
+# Start the web server
+echo "Starting web server..."
 exec init-web
-
-echo "All set, awaiting commands."
-sleep infinity;
